9abf3027d3d62181bf171ac19803ee285563a363
Ankit Jha <ankitjha1st@gmail.com>
2022-12-04 09:59:48 +0530

    Initial commit with some notes

---
+++ Notes.js
@@ -0,0 +1,17 @@
+/* Node js Notes
+
+
+    - No window 
+        means we will never write “document.querySelector”
+
+    - Versions 
+        - Node js app can run in a certain version, we do not need to update it(might give bugs if we do), 
+        - but if built for browsers, the browser can upgrade and can break the app
+
+    - node has 2 versions
+        - REPL
+            - type "node" in terminal and go to REPL mode 
+        - CLI 
+
+
+*/
